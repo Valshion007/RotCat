dd7678849bed0b6a5e4ded01a4c8de7aa9c4e398
Moon613 <68190337+Moon613@users.noreply.github.com>
2023-03-26 00:48:42 -0400

    Update RotCatOptions.cs

--- src/plugin/RotCatOptions.cs
+++ src/plugin/RotCatOptions.cs
@@ -6,18 +6,10 @@ using UnityEngine;
 namespace RotCat;
 public class RotCatOptions : OptionInterface {
     private readonly ManualLogSource Logger;
-    public readonly Configurable<int> poleShockRangeX = new Configurable<int>(700);
-    public readonly Configurable<int> poleShockRangeY = new Configurable<int>(400);
-    public readonly Configurable<int> poleZapFrequency = new Configurable<int>(10);
-    public readonly Configurable<bool> allowPoleZaps = new Configurable<bool>(false);
     private UIelement[] UIArrPlayerOptions;
 
     public RotCatOptions(RotCat pluginInstance, ManualLogSource logSource) {
         Logger = logSource;
-        poleShockRangeX = config.Bind("poleShockRangeX", 700, new ConfigAcceptableRange<int>(1, 1400));
-        poleShockRangeY = config.Bind("poleShockRangeY", 400, new ConfigAcceptableRange<int>(1, 800));
-        poleZapFrequency = config.Bind("poleZapFrequency", 10, new ConfigAcceptableRange<int>(1, 50));
-        allowPoleZaps = config.Bind("allowPoleZaps", true, (ConfigurableInfo)null);
     }
 
     public override void Initialize() {
@@ -30,18 +22,6 @@ public class RotCatOptions : OptionInterface {
         UIArrPlayerOptions = new UIelement[]
         {
             new OpLabel(260f, 570f, "Options", true),
-            
-            new OpLabel(150f, 520f, "Pole Shock Range Horizontal"),
-            new OpUpdown(poleShockRangeX, new Vector2(330f,515f), 80f) {description = "Determines the length of pole ZLLs can shock on the horizontal. 700 for 1 screen wide (assuming start is in the middle of the room)."},
-            
-            new OpLabel(150f, 480f, "Pole Shock Range Vertical"),
-            new OpUpdown(poleShockRangeY, new Vector2(330f,475f), 80f) {description = "Determines the length of pole ZLLs can shock on the vertical. 400 for 1 screen tall (assuming start is in the middle of the room)."},
-             
-            new OpLabel(150f, 440f, "Frequency of Pole Zaps"),
-            new OpUpdown(poleZapFrequency, new Vector2(330f,435f), 80f) {description = "The frequency at which ZLLs will zap poles"},
-
-            new OpLabel(180f, 400f, "Allow Pole Zaps"),
-            new OpCheckBox(allowPoleZaps, new Vector2(360f,400f)) { description = "Allows ZLLs to electrocute poles on touch" },
            
         };
         opTab.AddItems(UIArrPlayerOptions);
