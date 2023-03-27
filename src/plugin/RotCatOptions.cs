9663c10190caf0b7a4404776da68d4bdf01bbefd
Moon613 <68190337+Moon613@users.noreply.github.com>
2023-03-26 23:40:26 -0400

    Update list of bugs/errors

--- src/plugin/RotCatOptions.cs
+++ src/plugin/RotCatOptions.cs
@@ -10,7 +10,7 @@ public class RotCatOptions : OptionInterface {
     public readonly Configurable<KeyCode> tentMovementDown = new Configurable<KeyCode>(KeyCode.S);
     public readonly Configurable<KeyCode> tentMovementLeft = new Configurable<KeyCode>(KeyCode.A);
     public readonly Configurable<KeyCode> tentMovementRight = new Configurable<KeyCode>(KeyCode.D);
-    public readonly Configurable<KeyCode> tentMovementEnable = new Configurable<KeyCode>(KeyCode.LeftShift);
+    public readonly Configurable<KeyCode> tentMovementEnable = new Configurable<KeyCode>(KeyCode.LeftAlt);
     private UIelement[] UIArrPlayerOptions;
 
     public RotCatOptions(RotCat pluginInstance, ManualLogSource logSource) {
@@ -19,7 +19,7 @@ public class RotCatOptions : OptionInterface {
         tentMovementDown = config.Bind("tentacleMovementDown", KeyCode.S);
         tentMovementLeft = config.Bind("tentacleMovementLeft", KeyCode.A);
         tentMovementRight = config.Bind("tentacleMovementRight", KeyCode.D);
-        tentMovementEnable = config.Bind("tentacleMovementEnable", KeyCode.LeftShift);
+        tentMovementEnable = config.Bind("tentacleMovementEnable", KeyCode.LeftAlt);
     }
 
     public override void Initialize() {
@@ -34,11 +34,16 @@ public class RotCatOptions : OptionInterface {
             new OpLabel(260f, 570f, "Options", true),
 
             new OpLabel(200f, 520f, "Movement Keybinds", true),
-            new OpKeyBinder(tentMovementUp, new Vector2(250f, 480f), new Vector2(100f, 30f)) ,
-            new OpKeyBinder(tentMovementDown, new Vector2(250f, 445f), new Vector2(100f, 30f)),
-            new OpKeyBinder(tentMovementLeft, new Vector2(250f, 410f), new Vector2(100f, 30f)),
-            new OpKeyBinder(tentMovementRight, new Vector2(250f, 375f), new Vector2(100f, 30f)),
-            new OpKeyBinder(tentMovementEnable, new Vector2(250f, 340f), new Vector2(100f, 30f))
+            new OpLabel(50f, 485f, "Movement Tentacles Up", false),
+            new OpKeyBinder(tentMovementUp, new Vector2(250f, 480f), new Vector2(100f, 30f)) {description = "Tentacle Up"},
+            new OpLabel(50f, 450f, "Movement Tentacles Down", false),
+            new OpKeyBinder(tentMovementDown, new Vector2(250f, 445f), new Vector2(100f, 30f)) {description = "Tentacle Down"},
+            new OpLabel(50f, 415f, "Movement Tentacles Left", false),
+            new OpKeyBinder(tentMovementLeft, new Vector2(250f, 410f), new Vector2(100f, 30f)) {description = "Tentacle Left"},
+            new OpLabel(50f, 380f, "Movement Tentacles Right", false),
+            new OpKeyBinder(tentMovementRight, new Vector2(250f, 375f), new Vector2(100f, 30f)) {description = "Tentacle Right"},
+            new OpLabel(50f, 345f, "Activate the tentacle movement", false),
+            new OpKeyBinder(tentMovementEnable, new Vector2(250f, 340f), new Vector2(100f, 30f)) {description = "Enable movement via tentacles"}
            
         };
         opTab.AddItems(UIArrPlayerOptions);
